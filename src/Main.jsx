58961451f063550cb12ee08f05ceae94373c587d
w@rren <w@rren.dev>
2022-06-17 12:17:23 +0100

    data

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,56 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+const data = [
+  {
+    time: "05:03",
+    name: "Melisent Natica",
+  },
+  {
+    time: "02:04",
+    name: "Vevay Gale",
+  },
+  {
+    time: "05:02",
+    name: "Tabbatha Francyne",
+  },
+  {
+    time: "10:11",
+    name: "Tina Christine",
+  },
+  {
+    time: "01:03",
+    name: "Jobi Shirberg",
+  },
+  {
+    time: "09:11",
+    name: "Berget Rona",
+  },
+  {
+    time: "07:05",
+    name: "Harrietta Callista",
+  },
+  {
+    time: "06:12",
+    name: "Sarette Brodench",
+  },
+  {
+    time: "12:07",
+    name: "Genevra Annice",
+  },
+  {
+    time: "11:04",
+    name: "Gerianna Arvo",
+  },
+  {
+    time: "06:04",
+    name: "Joy Eno",
+  },
+  {
+    time: "07:07",
+    name: "Viviene Sikorski",
+  },
+];
+
 export function Main() {
   return (
     <>
@@ -7,14 +58,35 @@ export function Main() {
         <main className="nhsuk-main-wrapper" id="maincontent" role="main">
           <div className="nhsuk-grid-row">
             <div className="nhsuk-grid-column-two-thirds">
-              <h1 className="nhsuk-heading-l focusable-header" tabIndex="-1">
-                Get ready to list
-              </h1>
+              <h1>Today's appointments</h1>
+              <table class="nhsuk-table">
+                {/* <caption class="nhsuk-table__caption">
+                  List of appointments for today
+                </caption> */}
+                <thead role="rowgroup" class="nhsuk-table__head">
+                  <tr role="row">
+                    <th role="columnheader" class="" scope="col">
+                      Time
+                    </th>
+                    <th role="columnheader" class="" scope="col">
+                      Name
+                    </th>
+                    <th role="columnheader" class="" scope="col">
+                      Check in
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="nhsuk-table__body">
+                  {data.map(({ name, time }) => (
+                    <Row time={time} name={name} />
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </main>
       </div>
-      <Router>
+      {/* <Router>
         <h1>Hello</h1>
         <ul>
           <li>
@@ -27,18 +99,29 @@ export function Main() {
             <Link to="/users">Users</Link>
           </li>
         </ul>
-        <Switch>
-          <Route path="/about">
-            <p>about</p>
-          </Route>
-          <Route path="/users">
-            <p>users</p>
-          </Route>
-          <Route path="/">
-            <p>home</p>
-          </Route>
-        </Switch>
-      </Router>
+
+        <Route path="/about">
+          <p>about</p>
+        </Route>
+        <Route path="/users">
+          <p>users</p>
+        </Route>
+        <Route path="/">
+          <p>home</p>
+        </Route>
+      </Router> */}
     </>
+  );
+}
+
+function Row({ time, name }) {
+  return (
+    <tr role="row" class="nhsuk-table__row">
+      <td class="nhsuk-table__cell">{time}</td>
+      <td class="nhsuk-table__cell">{name}</td>
+      <td class="nhsuk-table__cell">
+        <a href="#">Check in</a>
+      </td>
+    </tr>
   );
 }
