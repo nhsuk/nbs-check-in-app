bc61f0d4530edaffe52fa4cd45db20184eb24f0c
w@rren <w@rren.dev>
2022-06-17 13:29:06 +0100

    journey

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,52 +2,88 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const data = [
   {
-    time: "05:03",
-    name: "Melisent Natica",
+    time: "01:08",
+    firstname: "Pearline",
+    lastname: "Margret",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "02:04",
-    name: "Vevay Gale",
+    time: "05:08",
+    firstname: "Charlena",
+    lastname: "Meter",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "05:02",
-    name: "Tabbatha Francyne",
+    time: "09:05",
+    firstname: "Jerry",
+    lastname: "Ahab",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "10:11",
-    name: "Tina Christine",
+    time: "02:10",
+    firstname: "Beth",
+    lastname: "McClimans",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "01:03",
-    name: "Jobi Shirberg",
+    time: "10:07",
+    firstname: "Ileana",
+    lastname: "Rugen",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "09:11",
-    name: "Berget Rona",
+    time: "08:12",
+    firstname: "Jeanna",
+    lastname: "Stoller",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "07:05",
-    name: "Harrietta Callista",
+    time: "04:12",
+    firstname: "Sherrie",
+    lastname: "Gabrielli",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "06:12",
-    name: "Sarette Brodench",
+    time: "12:09",
+    firstname: "Tera",
+    lastname: "Tybald",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "12:07",
-    name: "Genevra Annice",
+    time: "10:05",
+    firstname: "Lonnie",
+    lastname: "Ranjiv",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "11:04",
-    name: "Gerianna Arvo",
+    time: "12:09",
+    firstname: "Devina",
+    lastname: "Selway",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "06:04",
-    name: "Joy Eno",
+    time: "06:06",
+    firstname: "Modestia",
+    lastname: "Phaidra",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
   {
-    time: "07:07",
-    name: "Viviene Sikorski",
+    time: "02:10",
+    firstname: "Tera",
+    lastname: "Jagir",
+    bookingReference: "ABF3235234",
+    nhsNumber: "4859375849",
   },
 ];
 
@@ -58,30 +94,45 @@ export function Main() {
         <main className="nhsuk-main-wrapper" id="maincontent" role="main">
           <div className="nhsuk-grid-row">
             <div className="nhsuk-grid-column-two-thirds">
-              <h1>Today's appointments</h1>
-              <table class="nhsuk-table">
-                {/* <caption class="nhsuk-table__caption">
-                  List of appointments for today
-                </caption> */}
-                <thead role="rowgroup" class="nhsuk-table__head">
-                  <tr role="row">
-                    <th role="columnheader" class="" scope="col">
-                      Time
-                    </th>
-                    <th role="columnheader" class="" scope="col">
-                      Name
-                    </th>
-                    <th role="columnheader" class="" scope="col">
-                      Check in
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="nhsuk-table__body">
-                  {data.map(({ name, time }) => (
-                    <Row time={time} name={name} />
-                  ))}
-                </tbody>
-              </table>
+              <Router>
+                <Switch>
+                  <Route path="/checked-in">
+                    <CheckedIn />
+                  </Route>
+                  <Route path="/check-in">
+                    <>
+                      <h1>Select the person you want to check in</h1>
+                      <CheckIn />
+                    </>
+                  </Route>
+                  <Route path="/">
+                    <h1>Select the person you want to check in</h1>
+                    <div class="nhsuk-form-group">
+                      <label class="nhsuk-label" for="example">
+                        Search by name or booking reference
+                      </label>
+                      <input
+                        class="nhsuk-input nhsuk-u-width-three-quarters"
+                        id="example"
+                        name="example"
+                        type="text"
+                      />
+                      <br />
+                      <br />
+                      <button className="nhsuk-button">Search</button>
+                      <h2>Today's appointments</h2>
+                      <hr />
+                    </div>
+
+                    {/* <p>Select the person you wish to check in</p> */}
+                    <ul className="nhsuk-list">
+                      {data.map((item) => (
+                        <Row {...item} />
+                      ))}
+                    </ul>
+                  </Route>
+                </Switch>
+              </Router>
             </div>
           </div>
         </main>
@@ -114,14 +165,54 @@ export function Main() {
   );
 }
 
-function Row({ time, name }) {
+function Row({ time, firstname, lastname, bookingReference, nhsNumber }) {
   return (
-    <tr role="row" class="nhsuk-table__row">
-      <td class="nhsuk-table__cell">{time}</td>
-      <td class="nhsuk-table__cell">{name}</td>
-      <td class="nhsuk-table__cell">
-        <a href="#">Check in</a>
-      </td>
-    </tr>
+    <li className="nhsuk-u-margin-bottom-6">
+      <Link to="/check-in">
+        <b>{lastname}</b>, {firstname}
+      </Link>
+    </li>
+  );
+}
+
+function CheckIn() {
+  return (
+    <>
+      <dl class="nhsuk-summary-list">
+        <div class="nhsuk-summary-list__row">
+          <dt class="nhsuk-summary-list__key">Name</dt>
+          <dd class="nhsuk-summary-list__value">Melisent Natica</dd>
+        </div>
+
+        <div class="nhsuk-summary-list__row">
+          <dt class="nhsuk-summary-list__key">Time</dt>
+          <dd class="nhsuk-summary-list__value">5:03</dd>
+        </div>
+
+        <div class="nhsuk-summary-list__row">
+          <dt class="nhsuk-summary-list__key">Booking reference</dt>
+          <dd class="nhsuk-summary-list__value">AVFD585849</dd>
+        </div>
+
+        <div class="nhsuk-summary-list__row">
+          <dt class="nhsuk-summary-list__key">NHS Number</dt>
+          <dd class="nhsuk-summary-list__value">3446235432</dd>
+        </div>
+      </dl>
+      <Link to="/checked-in" className="nhsuk-button">
+        Check this person in
+      </Link>
+    </>
+  );
+}
+
+function CheckedIn() {
+  return (
+    <>
+      <h1>This person has been checked in</h1>
+      <Link to="/" className="nhsuk-button">
+        Back to start
+      </Link>
+    </>
   );
 }
